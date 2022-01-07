3f1557d75fd364c1a107597fff6890668a99debd
hfg-joschua-r <joschua.rothenbacher@hfg.design>
2022-01-07 17:00:02 +0100

    ImageUploadWorking:)

--- GTLH/ArduinoCode/takeAndSendImg.ino/takeAndSendImg.ino.ino
+++ GTLH/ArduinoCode/takeAndSendImg.ino/takeAndSendImg.ino.ino
@@ -1,20 +1,21 @@
-#include "esp_http_client.h"
-#include "esp_camera.h"
-#include <WiFi.h>
 #include "Arduino.h"
+#include "esp_camera.h"
 #include "config.h"
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <base64.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PSD;
-int capture_interval = 2000; // Microseconds between captures
-const char *post_url = POST_URL; // Location where images are POSTED
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE  // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
-bool internet_connected = false;
-long current_millis;
-long last_capture_millis = 0;
-
-// CAMERA_MODEL_AI_THINKER
+//CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
@@ -32,16 +33,17 @@ long last_capture_millis = 0;
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-HTTPClient http;
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PSD;
 
-void setup()
-{
+const int timerInterval = 30000;    // time between each HTTP POST image
+unsigned long previousMillis = 0;   // last time image was sent
+
+
+void setup() {
   Serial.begin(115200);
-
-  if (init_wifi()) { // Connected to WiFi
-    internet_connected = true;
-    Serial.println("Internet connected");
-  }
+  Serial.setDebugOutput(true);
+  Serial.println();
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -64,16 +66,23 @@ void setup()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if (psramFound()) {
-    config.frame_size = FRAMESIZE_UXGA;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_CIF;
     config.jpeg_quality = 10;
     config.fb_count = 2;
   } else {
-    config.frame_size = FRAMESIZE_SVGA;
+    config.frame_size = FRAMESIZE_QVGA;
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
@@ -81,104 +90,74 @@ void setup()
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-}
 
-bool init_wifi()
-{
-  int connAttempts = 0;
-  Serial.println("\r\nConnecting to: " + String(ssid));
+
+  #if defined(CAMERA_MODEL_M5STACK_WIDE)
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
+  #endif
+
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED ) {
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    if (connAttempts > 10) return false;
-    connAttempts++;
   }
-  return true;
-}
-
-
-esp_err_t _http_event_handler(esp_http_client_event_t *evt)
-{
-  switch (evt->event_id) {
-    case HTTP_EVENT_ERROR:
-      Serial.println("HTTP_EVENT_ERROR");
-      break;
-    case HTTP_EVENT_ON_CONNECTED:
-      Serial.println("HTTP_EVENT_ON_CONNECTED");
-      break;
-    case HTTP_EVENT_HEADER_SENT:
-      Serial.println("HTTP_EVENT_HEADER_SENT");
-      break;
-    case HTTP_EVENT_ON_HEADER:
-      Serial.println();
-      Serial.printf("HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
-      break;
-    case HTTP_EVENT_ON_DATA:
-      Serial.println();
-      Serial.printf("HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-      if (!esp_http_client_is_chunked_response(evt->client)) {
-        // Write out data
-        // printf("%.*s", evt->data_len, (char*)evt->data);
-      }
-      break;
-    case HTTP_EVENT_ON_FINISH:
-      Serial.println("");
-      Serial.println("HTTP_EVENT_ON_FINISH");
-      break;
-    case HTTP_EVENT_DISCONNECTED:
-      Serial.println("HTTP_EVENT_DISCONNECTED");
-      break;
-  }
-  return ESP_OK;
-}
-
-static esp_err_t take_send_photo()
-{
-  Serial.println("Taking picture...");
-  camera_fb_t * fb = NULL;
-  esp_err_t res = ESP_OK;
-
-  fb = esp_camera_fb_get();
-  if (!fb) {
-    Serial.println("Camera capture failed");
-    return ESP_FAIL;
-  }
-
-  esp_http_client_handle_t http_client;
+  Serial.println("");
+  Serial.println("WiFi connected");
   
-  esp_http_client_config_t config_client = {0};
-  config_client.url = post_url;
-  config_client.event_handler = _http_event_handler;
-  config_client.method = HTTP_METHOD_POST;
+  sendPhoto();
 
-  http_client = esp_http_client_init(&config_client);
+  //Serial.println("\nSleep....");
+  //esp_deep_sleep_start();
+ 
+}
 
-  esp_http_client_set_post_field(http_client, (const char *)fb->buf, fb->len);
+void loop(){
 
-  esp_http_client_set_header(http_client, "Content-Type", "image/jpg");
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= timerInterval) {
+    sendPhoto();
+    previousMillis = currentMillis;
+  }
+}
 
-  esp_err_t err = esp_http_client_perform(http_client);
-  if (err == ESP_OK) {
-    Serial.print("esp_http_client_get_status_code: ");
-    Serial.println(esp_http_client_get_status_code(http_client));
+void sendPhoto(){
+  String response;
+  
+  // Capture picture
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+   
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  } else {
+    Serial.println("Camera capture OK");
   }
 
-  esp_http_client_cleanup(http_client);
+  size_t size = fb->len;
+  String buffer = base64::encode((uint8_t *) fb->buf, fb->len);
+  
+  String imgPayload = "{\"base64\": \"" + buffer + "\"}";
+
+  buffer = "";
+  // Uncomment this if you want to show the payload
+  Serial.println(imgPayload);
 
   esp_camera_fb_return(fb);
-}
-
-
-
-void loop()
-{
-  // TODO check Wifi and reconnect if needed
   
-  current_millis = millis();
-  if (current_millis - last_capture_millis > capture_interval) { // Take another picture
-    last_capture_millis = millis();
-    take_send_photo();
+  HTTPClient http;
+  http.begin(POST_URL);
+  http.addHeader("Content-Type", "application/json");      
+  int httpResponseCode = http.POST(imgPayload);
 
-  }
+  if(httpResponseCode>0){
+    Serial.print(httpResponseCode);
+    Serial.print(" Returned String: ");
+    Serial.println(http.getString());
+  } else {      
+    Serial.print("POST Error: ");
+    Serial.print(httpResponseCode);
+  }                      
 }
--- GTLH/dataservice/index.js
+++ GTLH/dataservice/index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan")
 const path = require('path');
 const dataService = express();
 const port = process.env.DATASERVICE_PORT;
+const fs = require("fs");
 
 dataService.use(express.json());
 dataService.use(morgan('dev'));
@@ -19,25 +20,44 @@ const imageUpload = multer({
                 cb(
                     null,
                     new Date().valueOf() + 
-                    '_' +
-                    file.originalname
+                    '_sentImg'
                 );
             }
         }
     ), 
 });
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+
 //API ENDPOINTS
 dataService.get("/", (req, res) => {
     res.send("Welcome to GrowTimeLapseHelper");
 });
-
 dataService.listen(port, () => {
     console.log(`GrowTimeLapseHelper dataService listening at http://127.0.0.1:${port}`);
 });
-dataService.post("/api/imageUpload", imageUpload.single('image'), (req, res) => { 
-    console.log(req.file);
-    res.json('/image api'); 
+dataService.post("/api/imageUpload2", imageUpload.single('image'), (req, res) => { 
+    //console.log(req);
+    console.log(JSON.stringify(req.file));
+    res.status(200).json('/image api'); 
 });
+dataService.post("/api/imageUpload", (req, res) => { 
+    //console.log(JSON.stringify(req.body.base64));
+    let buffer = req.body.base64;
+    let buf = Buffer.from(buffer, 'base64'); // Ta-da
+    console.log(buf)
+    fs.writeFileSync("./images/newest.jpg", buf);
+    res.status(200).send('/image api'); 
+});
+
 dataService.get('/image/:filename', (req, res) => {
     const { filename } = req.params;
     const dirname = path.resolve();
